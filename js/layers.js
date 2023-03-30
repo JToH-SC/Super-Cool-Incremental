eeb18a99cc8e4f4aa4e3e13d3029e3962ff1159c
JToH-SC <129375054+JToH-SC@users.noreply.github.com>
2023-03-30 17:58:15 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -59,7 +59,7 @@ addLayer("p", {
 addLayer("s", {
     name: "super", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: false,
 		points: new Decimal(20),
