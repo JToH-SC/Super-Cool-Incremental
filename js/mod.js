302d5baeff23422dacdd9303d2892d7ef65e96c8
sdda <129375054+JToH-SC@users.noreply.github.com>
2023-05-26 19:25:34 +0800

    qol

--- js/mod.js
+++ js/mod.js
@@ -21,7 +21,7 @@ let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.211</h3><br>
 		- Added an achievement layer, with 8 achievements.<br>
 		- Added new QoL for Power, Intensity, and Control.<br>
-		- Nerfed Control 23.<brs>
+		- Nerfed Control 23.<br>
 	<h3>v0.21</h3><br>
 		- Added 3 new layers, "Knowledge", "Practice", and "Wisdom".<br>
 		- Added a layer effect to "Knowledge".<br>
